e8f7c08946c5d61032a78ed21a29e417ccb33c61
DannyArends <Danny.Arends@gmail.com>
2019-05-30 19:29:16 +0100

    Builing environmental settings

--- R/PCC.R
+++ R/PCC.R
@@ -3,12 +3,20 @@
 #
 
 PCC <- function(aM, bM) {
-  res <- .C("R_pcc_matrix", aM = as.double(x),
-                            bM = as.double(x),
+  res <- .C("R_pcc_matrix", aM = as.double(aM),
+                            bM = as.double(bM),
                             n = as.integer(ncol(aM)), # nInd
                             m = as.integer(nrow(aM)), # nPhe A
                             p = as.integer(nrow(bM)), # nPhe B
-                            res = as.double(rep(0, nrow(aM) * nrow(bM))), PACKAGE="MPCC")
+                            res = as.double(rep(0, nrow(aM) * nrow(bM))), package = "MPCC")
   return(res)
+}
+
+test <- function(){
+    library(MPCC)
+    set.seed(1)
+    mA <- matrix(runif(100),10,10) 
+    mB <- matrix(runif(100),10,10) 
+    PCC(mA, mB)
 }
 
--- building.sh
+++ building.sh
@@ -1,3 +1,5 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/compilers_and_libraries_2018/linux/mkl/lib/intel64/:/opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/lib/intel64
+
 R CMD INSTALL --configure-args='--with-mpcc-include=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/include/ --with-mpcc-lib=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/lib/intel64/' MPCC
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/lib/intel64/
+
