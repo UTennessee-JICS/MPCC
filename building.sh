f9eb755621255680d34454375430d0ce771bad7e
DannyArends <Danny.Arends@gmail.com>
2019-05-31 16:49:34 +0100

    Minor more updates

--- building.sh
+++ building.sh
@@ -1,3 +1,8 @@
+# Go inside the repository and run
+#$ autoconf
+
+# Go outside the repository and run to compile
+
 R CMD INSTALL --configure-args='--with-mpcc-include=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/include/ --with-mpcc-lib=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/' MPCC
 
 
