625b98e6f95ed293facd14dfaec30ce1bf763d78
DannyArends <Danny.Arends@gmail.com>
2019-05-31 16:16:45 +0100

    Updates to R, making pretty graphs

--- R/PCC.R
+++ R/PCC.R
@@ -8,36 +8,75 @@ PCC <- function(aM, bM) {
                             n = as.integer(ncol(aM)), # nInd
                             m = as.integer(nrow(aM)), # nPhe A
                             p = as.integer(nrow(bM)), # nPhe B
-                            res = as.double(rep(0, nrow(aM) * nrow(bM))), package = "MPCC")
+                            res = as.double(rep(0, nrow(aM) * nrow(bM))), NAOK = TRUE, package = "MPCC")
   return(res)
 }
 
-test <- function(){
-    library(MPCC)
+test <- function() {
+    require(MPCC)
     set.seed(1)
+
+    # Percentage of missing data
+    missing = 0.05
+
     times.ref <- c()
+    times.ref.missing <- c()
     times.pcc <- c()
-    sizes <- c(100, 150, 200, 250, 300, 400, 500, 1000)
+    sizes <- c(50, 100, 150, 200, 250, 300, 400, 500, 750, 1000)
     for(x in sizes) {
-      time.ref <- 0
-      time.pcc <- 0
-      for(y in 1:10) {
-        mA <- matrix(runif(x*x),x,x) 
-        mB <- matrix(runif(x*x),x,x) 
-        s.ref <- proc.time()[3]                             # Start time
+      time.ref <- c()
+      time.ref.missing <- c()
+      time.pcc <- c()
+      for(y in 1:25) {
+        mA <- matrix(runif(x * x), x, x)
+        mB <- matrix(runif(x * x), x, x)
+        nmis <- (x * x * missing)
+        s.ref <- proc.time()[3] # Start time
         cor(mA, mB)
-        time.ref <- time.ref + (proc.time()[3] - s.ref)     # Add time information
-        s.pcc <- proc.time()[3]                             # Start time
+        time.ref <- c(time.ref, (proc.time()[3] - s.ref)) # Add time information
+        s.pcc <- proc.time()[3] # Start time
         PCC(mA, mB)
-        time.pcc <- time.pcc + (proc.time()[3] - s.pcc)     # Add time information
+        time.pcc <- c(time.pcc, (proc.time()[3] - s.pcc)) # Add time information
+
+        mA[sample(x*x, nmis)] <- NA
+
+        s.ref.missing <- proc.time()[3] # Start time
+        cor(mA, mB, use = "pair")
+        time.ref.missing <- c(time.ref.missing, (proc.time()[3] - s.ref.missing)) # Add time information
       }
-      times.ref <- c(times.ref, time.ref)
-      times.pcc <- c(times.pcc, time.pcc)
+      times.ref <- cbind(times.ref, time.ref)
+      times.pcc <- cbind(times.pcc, time.pcc)
+      times.ref.missing <- cbind(times.ref.missing, time.ref.missing)
       cat("Done", x, "\n")
     }
-    mY <- max(c(times.ref, times.pcc))
-    plot(c(min(sizes), max(sizes)), c(0, mY), t = 'n')
-    points(sizes, times.ref, t = 'l', col="orange")
-    points(sizes, times.pcc, t = 'l', col="green")
+    colnames(times.ref) <- sizes
+    colnames(times.pcc) <- sizes
+    colnames(times.ref.missing) <- sizes
+
+    # Calculate the numbers median / stdev
+    ref.med <- apply(times.ref, 2, median)
+    ref.sd <- apply(times.ref, 2, sd)
+
+    pcc.med <- apply(times.pcc, 2, median)
+    pcc.sd <- apply(times.pcc, 2, sd)
+
+    ref.med.missing <- apply(times.ref.missing, 2, median)
+    ref.sd.missing <- apply(times.ref.missing, 2, sd)
+
+    # Make the plot
+    mY <- max(c(times.ref.missing, times.ref, times.pcc))
+    plot(c(min(sizes), max(sizes)), c(0, mY), t = 'n', xlab = "size", ylab = "time (s)")
+
+    polygon(x = c(sizes, rev(sizes)), y = c(ref.med + ref.sd, rev(ref.med - ref.sd)), col="blue")
+    points(x = sizes, y = ref.med, t = 'l', col="blue", lwd=2)
+
+    polygon(x = c(sizes, rev(sizes)), y = c(pcc.med + pcc.sd, rev(pcc.med - pcc.sd)), col="orange")
+    points(x = sizes, y = pcc.med, t = 'l', col="orange", lwd=2)
+
+    polygon(x = c(sizes, rev(sizes)), 
+            y = c(ref.med.missing + ref.sd.missing, rev(ref.med.missing - ref.sd.missing)), col="lightblue")
+    points(x = sizes, y = ref.med.missing, t = 'l', col="lightblue", lwd=2)
+
+    legend("topleft", c("R cor()", "R cor(5% missing)", "Matrix PCC"), fill = c("blue", "lightblue", "orange"))
 }
 
