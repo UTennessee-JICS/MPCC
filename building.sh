682e1d622538fdb1d4ba5bfc3ebd557b041b067d
DannyArends <Danny.Arends@gmail.com>
2019-06-04 07:31:48 +0100

    Using a single mkl_home variable for configuring mkl

--- building.sh
+++ building.sh
@@ -3,6 +3,4 @@
 
 # Go outside the repository and run to compile
 
-R CMD INSTALL --configure-args='--with-mpcc-include=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/include/ --with-mpcc-lib=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/' MPCC
-
-
+R CMD INSTALL --configure-args='--with-mkl-home=/opt/intel/compilers_and_libraries_2018.2.199/linux/mkl/' MPCC
