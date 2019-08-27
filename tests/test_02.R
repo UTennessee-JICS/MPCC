54d4b8f7f047adbb96dd1d36535b6fb99baad159
DannyArends <Danny.Arends@Gmail.com>
2019-08-27 11:18:47 +0200

    Timing to test_02

--- tests/test_02.R
+++ tests/test_02.R
@@ -4,10 +4,20 @@
 library(MPCC)
 
 set.seed(1)
-mAB <- genAB(p = 5, n = 15, m = 10)
+mAB <- genAB(p = 2000, n = 2000, m = 2000)
 
+s.ref <- proc.time()[3]
 ref <- cor(mAB[["A"]], mAB[["B"]])
+(proc.time()[3] - s.ref)
+
+s.mpcc <- proc.time()[3]
 mpcc <- PCC(mAB[["A"]], mAB[["B"]])
+(proc.time()[3] - s.mpcc)
+
+s.mpcc <- proc.time()[3]
+mpcc <- PCC.naive(mAB[["A"]], mAB[["B"]])
+(proc.time()[3] - s.mpcc)
+
 
 if (sum(round(mpcc - ref, 12)) != 0) {
   stop("Inaccurate results for 5x10 matrix")
