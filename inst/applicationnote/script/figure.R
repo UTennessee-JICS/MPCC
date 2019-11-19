0ef317368dce72d2c6a9446d0205f6d847342ad8
DannyArends <Danny.Arends@Gmail.com>
2019-11-19 15:28:27 +0100

    Major rework of the paper (almost down to 2 pages)

--- inst/applicationnote/script/figure.R
+++ inst/applicationnote/script/figure.R
@@ -52,15 +52,29 @@ mean(time.ref) / mean(time.matrix)
 mean(time.ref) / mean(time.naive)
 
 onChr1 <- rownames(bxd.map[which(bxd.map[,"Chr"] == 1),])
+op <- par(cex=0.85)
 
 mypalette <- brewer.pal(9, "RdBu")
 cols <- colorRampPalette(mypalette)(200)
 image(x = as.numeric(bxd.map[onChr1, "Mb"]), y = as.numeric(bxd.map[onChr1, "Mb"]), z = res.matrix[onChr1,onChr1], 
-      breaks = seq(-1,1,0.01), col=cols, xaxt='n', yaxt='n', xlab="Position (Mb)", ylab="Position (Mb)", main="BXD genotype correlation (Chr 1)")
+      breaks = seq(-1,1,0.01), col=cols, xaxt='n', yaxt='n', xlab="Position (Mb)", ylab="Position (Mb)", main="BXD genotype correlation (Chromosome 1)")
 box()
 axis(1, at = seq(0, 195, 25), round(seq(0, 195, 25), 0))
 axis(2, at = seq(0, 195, 25), round(seq(0, 195, 25), 0), las=2)
-legend("bottomright", legend = format(round(seq(1, -1, -0.25),2), nsmall = 2), title = "Pearson's ρ", fill = rev(colorRampPalette(mypalette)(length(seq(-1, 1, 0.25)))), bg = "white", border = "white")
+legend("bottomright", legend = format(round(seq(1, -1, -0.5),2), nsmall = 2), fill = rev(colorRampPalette(mypalette)(length(seq(-1, 1, 0.5)))), bg = "white", border = "white")
+
+
+Speedup <- c(0, 25, 65, 75, 85, 95)
+Dimension <- c(10, 250, 1000, 2000, 4000, 8000)
+plot(c(0,9000), c(0, 100), t = 'n', xlab= "Matrix Dimensions (n^2)", ylab= "Speedup", main="Runtime speedup of MPCC versus cor()", xaxt='n', yaxt='n', xaxs='i', yaxs='i')
+axis(1, at = seq(0, 9000, 1000), seq(0, 9000, 1000), 0)
+axis(2, at = seq(0, 100, 20), seq(0, 100, 20), las=2)
+for(h in seq(20, 100, 20)){ abline(h = h, col="gray"); }
+for(v in seq(1000, 9000, 1000)){ abline(v = v, col="gray"); }
+points(x = Dimension, y = Speedup, t = 'b', pch=19)
+box()
+legend("bottomright", legend = c("5% missing"), lwd=1, pch=19, col="black", bg = "white", border = "white")
+
 
 #
 # Gene expression co-expression data for benchmarking speedup
