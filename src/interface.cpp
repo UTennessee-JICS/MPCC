90f8cfc2c9058966a82752b344dd3f1af0d474d2
DannyArends <Danny.Arends@Gmail.com>
2020-01-16 13:21:06 +0100

    Removed the fallback to pcc_naive, since pcc_matrix should now be independant of the MKL

--- src/interface.cpp
+++ src/interface.cpp
@@ -5,10 +5,11 @@ extern "C" {
   // Wrap the matrix version into a C call
   void R_pcc_matrix(double* aM, double* bM, int* nptr, int* mptr, int* pptr, double* res) {
     #ifndef NOMKL
+    info("[INFO] Library compiled with Intel MKL support: %d\n", 0);
     pcc_matrix((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
     #else
-    info("[WARNING] Library compiled with NO Intel MKL support: %d\n", 0);
-    pcc_naive((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
+    info("[INFO] Library compiled with openBLAS support: %d\n", 0);
+    pcc_matrix((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
     #endif
   }
 
