3dfed3908c52ceab575a53bed31f824e646542a5
DannyArends <Danny.Arends@Gmail.com>
2020-01-17 09:23:55 +0100

    Struggling to get this through the compiler

--- src/lapack.h
+++ src/lapack.h
@@ -1,17 +1,15 @@
-#ifndef __MPCC_LAPACK_H__
-  #define __MPCC_LAPACK_H__
+#ifndef MPCC_LAPACK_H
+#define MPCC_LAPACK_H
+#include <stddef.h>
 
-   #include <stddef.h> 
+enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
+enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
 
-  enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
-  enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
-
-  void dgemm_(const char *transa, const char *transb, const int *m, const int *n,
-              const int *k, const double *restrict alpha, const double *restrict a,
-              const int *lda, const double *restrict b, const int *ldb,
-              const double *beta, double *restrict c, const int *ldc);
-              
-  void daxpy_(const int N, const double alpha, const double *X,
-              const int incX, double *Y, const int incY);
-
+void dgemm_(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+                 const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
+                 const int K, const double alpha, const double *A,
+                 const int lda, const double *B, const int ldb,
+                 const double beta, double *C, const int ldc);
+void daxpy_(const int N, const double alpha, const double *X,
+                 const int incX, double *Y, const int incY);
 #endif
