dc93c52de0132e04af839a4d6d75117ade38cf72
DannyArends <Danny.Arends@Gmail.com>
2020-01-17 09:50:29 +0100

    revert NAMESPACE change, and adding comments / layout in lapack.h

--- src/lapack.h
+++ src/lapack.h
@@ -2,6 +2,7 @@
   #define MPCC_LAPACK_H
   #include <stddef.h>
 
+  // This file binds the external LAPACK/BLAS functionality we need by calling the Rstubs
   enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
   enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
 
