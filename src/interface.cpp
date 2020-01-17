b7b0e23084fcfe56d6de6009bd4093622d03dd6e
DannyArends <Danny.Arends@gmail.com>
2020-01-17 17:52:27 +0100

    Always call pcc_matrix, no message. ?fallback for some noBLAS setups?

--- src/interface.cpp
+++ src/interface.cpp
@@ -4,13 +4,7 @@ extern "C" {
 
   // Wrap the matrix version into a C call
   void R_pcc_matrix(double* aM, double* bM, int* nptr, int* mptr, int* pptr, double* res) {
-    #ifndef NOMKL
-    info("[INFO] Library compiled with Intel MKL support: %d\n", 0);
     pcc_matrix((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
-    #else
-    info("[INFO] Library compiled with openBLAS support: %d\n", 0);
-    pcc_matrix((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
-    #endif
   }
 
   // Wrap the naive version into a C call
