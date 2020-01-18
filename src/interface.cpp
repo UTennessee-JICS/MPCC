6c40cf84cebb5db8b6cb6c6c0cb77cd5c9c40e02
DannyArends <Danny.Arends@gmail.com>
2020-01-18 11:53:37 +0100

    Backend info function

--- src/interface.cpp
+++ src/interface.cpp
@@ -2,9 +2,26 @@
 
 extern "C" {
 
+  // C compute backend information
+  void R_backendinfo() {
+      #ifndef NOMKL
+        info("[INFO] Library compiled with Intel MKL support: %d\n", 1);
+      #else
+        #ifdef NOBLAS
+          info("[WARN] Library compiled with threading only: %d\n", 1);
+        #else
+          info("[INFO] Library compiled with R/BLAS support: %d\n", 1);
+        #endif
+      #endif
+  }
+
   // Wrap the matrix version into a C call
   void R_pcc_matrix(double* aM, double* bM, int* nptr, int* mptr, int* pptr, double* res) {
-    pcc_matrix((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
+    #ifdef NOBLAS
+      pcc_naive((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
+    #else
+      pcc_matrix((int)(*mptr), (int)(*nptr), (int)(*pptr), aM, bM, res);
+    #endif
   }
 
   // Wrap the naive version into a C call
