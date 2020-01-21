7cd1ac608913cd88ae6dc2fe3c664e21b351cd90
uditgulati <uditgulati0@gmail.com>
2020-01-22 04:06:21 +0530

    add test for m!=p with no missing data

---
+++ tests/test_06.R
@@ -0,0 +1,26 @@
+# copyright (c) - HU-Berlin / UTHSC / JICS by Danny Arends
+
+# Compare MPCC versus cor() function, mA = 15 x 5, mB = 15 x 10, no missing data
+library(MPCC)
+MPCCinfo()
+
+set.seed(1)
+mAB <- genAB(p = 200, n = 2000, m = 100)
+
+s.ref <- proc.time()[3]
+ref <- cor(mAB[["A"]], mAB[["B"]])
+(proc.time()[3] - s.ref)
+
+s.mpcc <- proc.time()[3]
+mpcc <- PCC(mAB[["A"]], mAB[["B"]])
+(proc.time()[3] - s.mpcc)
+
+s.mpcc <- proc.time()[3]
+mpcc <- PCC.naive(mAB[["A"]], mAB[["B"]])
+(proc.time()[3] - s.mpcc)
+
+
+if (sum(round(mpcc - ref, 12)) != 0) {
+  stop("Inaccurate results for 200x100 matrix")
+}
+
