a2105025236b6ea4e6c83235db167f5c791528c4
uditgulati <uditgulati0@gmail.com>
2020-01-22 04:00:52 +0530

    add test for all NA

---
+++ tests/test_05.R
@@ -0,0 +1,19 @@
+# copyright (c) - HU-Berlin / UTHSC / JICS by Danny Arends
+
+# Compare MPCC versus cor() function, mA = 15 x 5, mB = 15 x 10, no missing data
+library(MPCC)
+MPCCinfo()
+
+set.seed(1)
+mAB <- genAB(p = 5, n = 15, m = 10, missing = 1)
+
+ref <- cor(mAB[["A"]], mAB[["B"]], use="pair")
+mpcc <- PCC(mAB[["A"]], mAB[["B"]], debugOn=TRUE)
+
+mAB[["A"]]
+mAB[["B"]]
+
+if (sum(round(mpcc$res - ref, 12),na.rm = TRUE) != 0) {
+  stop("Inaccurate results for 5x10 matrix")
+}
+
