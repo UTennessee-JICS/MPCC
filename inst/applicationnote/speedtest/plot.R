034ddbfd136705d2786b6d4d90b32b0b7e39a8cb
DannyArends <Danny.Arends@Gmail.com>
2020-05-20 13:20:07 +0200

    Biger font size in the image

--- inst/applicationnote/speedtest/plot.R
+++ inst/applicationnote/speedtest/plot.R
@@ -1,10 +1,11 @@
 setwd("D:/Ddrive/Github/MPCC/inst/applicationnote/speedtest")
 mdata <- read.csv("MPCC_SpeedUp.txt", header = TRUE,sep="\t", skip = 2)
 
-op <- par(mar = c(5, 4.5, 4, 2) + 0.1)
+postscript("whatever.eps", width = 8, height = 6, paper = "special")
 
-plot(c(0, 10050), c(0, 220), t = 'n', xlab = 'shared dimension (n)', ylab = expression('Speedup '[(latency)]), xaxs = "i", yaxs = "i", 
-     main = "Speedup: MPCC(A, B) versus cor(A, B)", las=2, xaxt='n')
+op <- par(mar = c(5, 4.5, 0.5, 2) + 0.1)
+
+plot(c(0, 10050), c(0, 220), t = 'n', xlab = 'shared dimension (n)', ylab = expression('Speedup '[(latency)]), xaxs = "i", yaxs = "i", las=2, xaxt='n')
 axis(1, at = c(250,seq(1000,10000, 1000)), c(250,seq(1000,10000, 1000)))
 
 maxm <- max(unique(mdata[,"m"]))
@@ -29,14 +30,15 @@ for(m in c(250, 500, 1000, 1500, 2500)){
   
   x <- c(unique(inM[, "n"]), rev(unique(inM[, "n"])))
   #polygon(x, c(max.openblas, rev(min.openblas)),border = "blue", col=rgb(0,0,1,0.5), density = density, angle = angle)
+  for(x in 1:length(min.openblas)){
+    points(c(unique(inM[, "n"])[x], unique(inM[, "n"])[x]), c(smooth(mean.openblas)[x] - sd.openblas[x], smooth(mean.openblas)[x] + sd.openblas[x]), col=rgb(i/5,i/5,i/5,1), t = 'l', lwd=1, lty=1)
+  }
   if(i == 1){
     points(unique(inM[, "n"]), smooth(mean.openblas), col=rgb(0,1,0,1), t = 'l', lwd=i)
   }else{
     points(unique(inM[, "n"]), smooth(mean.openblas), col=rgb(0,0,1,1), t = 'l', lwd=i)
   }
-  for(x in 1:length(min.openblas)){
-    points(c(unique(inM[, "n"])[x], unique(inM[, "n"])[x]), c(smooth(mean.openblas)[x] - sd.openblas[x], smooth(mean.openblas)[x] + sd.openblas[x]), col=rgb(i/5,i/5,i/5,1), t = 'l', lwd=1, lty=3)
-  }
+
   density <- density - 10
   angle <- angle + 45
   i <- i + 0.5
@@ -60,16 +62,19 @@ for(m in c(2000, 3500)){
  
   x <- c(unique(inM[, "n"]), rev(unique(inM[, "n"])))
   #polygon(x, c(max.openblas, rev(min.openblas)),border = "blue", col=rgb(0,0,1,0.5), density = density, angle = angle)
-  points(unique(inM[, "n"]), smooth(mean.openblas), col=rgb(0,1,0,1), t = 'l', lwd=i)
   for(x in 1:length(min.openblas)){
-    points(c(unique(inM[, "n"])[x], unique(inM[, "n"])[x]), c(smooth(mean.openblas)[x] - sd.openblas[x], smooth(mean.openblas)[x] + sd.openblas[x]), col=rgb(i/3,i/3,i/3,1), t = 'l', lwd=1, lty=3)
+    points(c(unique(inM[, "n"])[x], unique(inM[, "n"])[x]), c(smooth(mean.openblas)[x] - sd.openblas[x], smooth(mean.openblas)[x] + sd.openblas[x]), col=rgb(i/3,i/3,i/3,1), t = 'l', lwd=1, lty=1)
   }
+  points(unique(inM[, "n"]), smooth(mean.openblas), col=rgb(0,1,0,1), t = 'l', lwd=i)
+
   density <- density - 10
   angle <- angle + 45
   i <- i + 0.5
 }
+op <- par(cex = 0.8)
 legend("topleft", c(paste0("A=", c(500, 1000, 1500, 2500), "*n, B=250*n"), "A=250*n, B=250*n", "A=2000*n, B=2000*n", "A=3500*n, B=3500*n"), lwd=c(seq(1,2.5,0.5), 1, 1.5, 2), col=c("blue", "blue", "blue", "blue", "green", "green", "green"))
 
+dev.off()
 
 #legend("topleft", c("openBLAS", "MKL"), fill = c(rgb(0,0,1,0.5), rgb(1,0,0,0.5)))
 
